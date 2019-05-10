fdd621374d9e89f7f24c61555ff80b11b16b845c
Samuel Urmy <urmy@shore.mbari.org>
2019-05-09 20:02:31 -0700

    Refactor model runs, packing them into a function to make changing params easier

--- src/run_model.jl
+++ src/run_model.jl
@@ -2,42 +2,72 @@ using Plots
 include("mpa_model.jl")
 m = MpaModel
 
-K = 1 # ton fish km^-2
-r = 0.5
-A = 100 # km^2
-α = 0.0
-μ = 0.01
+# Fishing ground parameters
+K = 1 # carrying capacity, ton fish km^-2
+r = 0.2 # Intrinsic growth rate of fish populations
+A = 100 # Area of fishing ground, km^2
+α0 = 0.0 # Proportion of fishing ground in MPA at start
+α100 = 0.3 # Proportion of fishing ground in MPA after 100 timesteps
+μ = 0.1 # reserve spillover parameter
 
-# both start at carrying capacity
-Nreserve = A * α * K
-Nopen = A * (1-α) * K
+# Fisher parameters
+a = .5 # Search rate, km^2 d^-1
+Th = 1 # Handling time, boat d (ton fish)^-1
+αpref = 0.0 # The fishers' preferred reserve fraction at beginning
+Δα = 0.01 # How fast the fishers adjust their preferred reserve size
+Nfishers = 20 # Number of fishers
 
-Nfishers = 200
-a = 30 # km^2 d^-1
-Th = 0.05 # boat d (ton fish)^-1
 
-fishers = [m.BasicFisher(a, Th)]
-ground = m.FishingGround(K, r, A, α, μ, Nreserve, Nopen)
+function run_simulation(params, nsim)
+    K, r, A, α0, α100, μ, a, Th, αpref, Δα, Nfishers = params
+    # Biomass in reserve and open areas. Both start at carrying capacity
+    Nreserve = A * α * K
+    Nopen = A * (1-α) * K
 
-nsim = 200
-NNres = zeros(nsim)
-NNopen = zeros(nsim)
-landings = zeros(nsim)
-for t in 1:nsim
-    NNres[t] = ground.Nreserve
-    NNopen[t] = ground.Nopen
-    if t == 50
-        global α = 0.2
-        m.set_protected!(ground, α)
+    # Instantiate an array of OpinionatedFishers
+    fishers = [m.OpinionatedFisher(a, Th, αpref, Δα) for i in 1:Nfishers]
+    # Instantiate a FishingGround
+    ground = m.FishingGround(K, r, A, α, μ, Nreserve, Nopen)
+
+    NNres = zeros(nsim)
+    NNopen = zeros(nsim)
+    landings = zeros(nsim)
+    αα_avg = zeros(nsim)
+
+    for t in 1:nsim
+        NNres[t] = ground.Nreserve
+        NNopen[t] = ground.Nopen
+        landings[t] = m.update!(ground, fishers)
+        if t == 100
+            m.set_protected!(ground, α100)
+            αα_avg[t] = α100
+        end
+        if t > 100
+            αα_avg[t] = m.α_consensus(fishers)
+            m.set_protected!(ground, αα_avg[t])
+        end
     end
-    landings[t] = m.update!(ground, fishers)
+    return (NNopen, NNres, landings, αα_avg)
 end
 
-p1 = plot(NNopen, label="Open biomass");
-plot!(p1, NNres, label="MPA biomass");
-plot!(p1, landings, label="Landings");
+function plot_results(NNopen, NNres, landings, αα_avg, burnin=1)
+    p1 = plot(NNopen[burnin:end], label="Open biomass");
+    plot!(p1, NNres[burnin:end], label="MPA biomass");
+    plot!(p1, landings[burnin:end], label="Landings");
+    p2 = plot(landings[burnin:end], label="Landings", linecolor=:green);
+    p3 = plot(αα_avg[burnin:end], label="MPA fraction", linecolor=:black);
+    p = plot(p1, p2, p3, layout=(3,1))
+    return p
+end
 
-p2 = plot(landings, label="Landings", );
-# plot!(p2, NNres, label="MPA biomass");
-# hline!(p2, [A * α * K], label="MPA B0")
-plot(p1, p2, layout=(2,1))
+params = (K=K, r=r, A=A, α0=α0, α100=α100, μ=μ,
+    a=a, Th=Th, αpref=αpref, Δα=Δα, Nfishers=Nfishers)
+nsim = 200
+res = run_simulation(params, nsim) #NNopen, NNres, landings, αα_avg
+plot_results(res...)
+plot_results(res..., 50)
+
+params = (K=K, r=r, A=A, α0=α0, α100=α100, μ=μ,
+    a=a, Th=0.5, αpref=αpref, Δα=Δα, Nfishers=Nfishers)
+res = run_simulation(params, nsim)
+plot_results(res..., 50)
